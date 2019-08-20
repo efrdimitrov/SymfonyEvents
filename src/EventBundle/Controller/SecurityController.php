ac7e2d9603bd60ce7296dff50fa689c5ade57a3f
efrdimitrov <efrdimitrov@abv.bg>
2019-08-20 22:02:05 +0300

    Edit: UserType

--- src/EventBundle/Controller/SecurityController.php
+++ src/EventBundle/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace EventBundle\Controller;
 
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Security\Core\User\User;
 
 class SecurityController extends Controller
 {
@@ -14,6 +15,21 @@ class SecurityController extends Controller
      */
     public function login()
     {
+        var_dump($this->login());
+
+//
+//        $em = $this->getDoctrine()->getManager();
+//        $allUsers = $em->getRepository(User::class)->findAll(array('username' => 'ASC'));
+//        if (in_array($user->getUsername(), $allUsers)) {
+//            $this->addFlash('exists_user', 'Username already exists!');
+//            return $this->redirectToRoute("user_register");
+//        }
+
+
+
+
+
+
         return $this->render('security/login.html.twig');
     }
 }
